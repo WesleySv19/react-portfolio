697446874a0475bdd1c6f5f97edd616c73cb474b
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2024-03-21 20:25:06 -0300

    adding my cv

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -28,7 +28,8 @@ const Home = () => {
       </div>
       
       <div data-aos="flip-up" className={styles.cv}>
-        <a href=""><button>Download CV</button></a>
+        <a 
+        href="https://drive.google.com/file/d/1FSAuRlptOPxDV6mKlNgLVyKENyIBng7r/view?usp=drive_link" target='_blank'><button>Download CV</button></a>
       </div>
 
     </section>
